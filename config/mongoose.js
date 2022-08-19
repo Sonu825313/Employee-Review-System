24b2b0c1c2a35e7669b5c093564c2980cbd7a028
Sonu825313 <sonukumarraj1341998@gmail.com>
2022-08-19 12:02:33 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,25 @@
+
+const mongoose = require('mongoose');
+
+
+// mongoose.connect('tabase?retryWrites=true&w=majority');
+let MongoDB_URL="mongodb+srv://sonukumar:Sonu6342@cluster0.cl4vgh9.mongodb.net/employee-review-system?retryWrites=true&w=majority";
+
+mongoose.connect(MongoDB_URL,{
+    useNewUrlParser: true,
+    //useFindAndModify: false,
+    useUnifiedTopology: true
+  });
+
+// aquire connection if it is succesful
+const db = mongoose.connection;
+
+// error
+db.on('error', console.error.bind(console, 'error connecting to db'));
+
+// up and running then print the message
+db.once('open', function(){
+    console.log("successfully connected to database!");
+});
+
+module.exports = db;
